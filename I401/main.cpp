af16628437ab6344e0d76033527fc312d1e2769a
YSRKEN <ysr.ken@gmail.com>
2016-04-10 10:17:12 +0900

    upload temporary code

--- I401/main.cpp
+++ I401/main.cpp
@@ -1,3 +1,4 @@
+#include<algorithm>
 #include<array>
 #include<cstdint>
 #include<iostream>
@@ -14,19 +15,40 @@ using std::vector;
 
 // const value declaration
 const size_t kBoardSize = 15;
+const size_t kSearchWidth = 5;
 const int kScoreInf = 1000;
 const string kPositionStringX = "abcdefghijklmno";
 const string kPositionStringY = "123456789ABCDEF";
+const size_t kPositionoffset[] = {1, kBoardSize, kBoardSize - 1, kBoardSize + 1};
 enum Stone : uint8_t {
 	None,
 	Black,
 	White
 };
+enum Direction : uint8_t {
+	// Row(Ñü) R[1, 0] L[-1, 0]
+	Row,
+	// Column(Ñ†) R[0, 1] L[0, -1]
+	Column,
+	// Diagonally right(Å^) R[-1, 1] L[1, -1]
+	DiagR,
+	// Diagonally left(Å_) R[1, 1] L[-1, -1]
+	DiagL,
+	Directions
+};
+enum Side : uint8_t {
+	Right,
+	Left,
+	Sides
+};
 
 // typedef declaration
 typedef std::pair<size_t, int> Score;
 typedef std::pair<size_t, bool> Result;
+typedef array<array<size_t, 5>, 2> Pattern;
+typedef array<array<array<size_t, Side::Sides>, Direction::Directions>, kBoardSize * kBoardSize> IterateTable;
 
+// definition in global area
 std::random_device rd;
 std::mt19937 mt(rd());
 
@@ -46,31 +68,10 @@ inline size_t RandInt(const size_t n) {
 
 // Board class
 class Board {
+	// values
 	array<Stone, kBoardSize * kBoardSize> board_;
 	Stone turn_;
-public:
-	// constructor
-	Board(const char board_text[], const char turn_text[]) {
-		const static string kStoneString = "-*O";
-		// Read board-text
-		const string board_text_str(board_text);
-		if (board_text_str.size() < kBoardSize * kBoardSize) {
-			throw std::exception("Too short board-text size!");
-		}
-		for (size_t p = 0; p < kBoardSize * kBoardSize; ++p) {
-			auto stone_type = kStoneString.find_first_of(board_text[p], 0);
-			if (stone_type == string::npos) {
-				throw std::exception("Can't read board data.");
-			}
-			board_[p] = static_cast<Stone>(stone_type);
-		}
-		// Read turn-text
-		auto turn_type = kStoneString.find_first_of(turn_text[0], 0);
-		if (turn_type == string::npos) {
-			throw std::exception("Can't read turn data.");
-		}
-		turn_ = static_cast<Stone>(turn_type);
-	}
+	IterateTable kIterateTable;
 	// Put board
 	void PutBoard() {
 		const static string kStoneString2[] = { "Ñ©", "Åú", "Åõ" };
@@ -137,7 +138,8 @@ public:
 				}
 				if (turn == Stone::Black) {
 					if (rlengh + llengh == 4) return Result(p, true);
-				}else{
+				}
+				else {
 					if (rlengh + llengh >= 4) return Result(p, true);
 				}
 			}
@@ -159,7 +161,8 @@ public:
 				}
 				if (turn == Stone::Black) {
 					if (rlengh + llengh == 4) return Result(p, true);
-				}else {
+				}
+				else {
 					if (rlengh + llengh >= 4) return Result(p, true);
 				}
 			}
@@ -256,6 +259,88 @@ public:
 		}
 		return Result(0, false);
 	}
+	// Get right and left pattern
+	Pattern GetPattern(const size_t position, const Direction dir) {
+		const size_t right_pattern_length = kIterateTable[position][dir][Side::Right];
+		const size_t  left_pattern_length = kIterateTable[position][dir][Side::Left ];
+		Pattern pattern;
+		auto GetBoardValue = [this](size_t position, Direction dir, int count) -> Stone {return board_[position + kPositionoffset[dir] * count]; };
+		auto GetBoardValue2 = [&GetBoardValue](size_t position, Direction dir, int count) -> Stone {
+			const auto get_value = GetBoardValue(position, dir, count);
+			return (get_value != Stone::White ? get_value : Stone::None);
+		};
+
+		auto nonBlack = [](const Stone stone) -> Stone {return (stone != Stone::White ? stone : Stone::None);};
+		pattern[Side::Right][0] = (right_pattern_length > 0 ? GetBoardValue2(position, dir, 1) : 0);
+		pattern[Side::Right][1] = (right_pattern_length > 1 ? GetBoardValue2(position, dir, 1) : 0);
+		pattern[Side::Right][2] = (right_pattern_length > 2 ? GetBoardValue2(position, dir, 1) : 0);
+		pattern[Side::Right][3] = (right_pattern_length > 3 ? GetBoardValue2(position, dir, 1) : 0);
+		pattern[Side::Right][4] = (right_pattern_length > 4 ? GetBoardValue2(position, dir, 1) : 0);
+
+		pattern[Side::Left][0] = 0;
+		pattern[Side::Left][1] = 0;
+		pattern[Side::Left][2] = 0;
+		pattern[Side::Left][3] = 0;
+		pattern[Side::Left][4] = 0;
+
+		return pattern;
+	}
+	// Find Shi-Oi tsume
+	Result FindShioiMove(const Stone turn) {
+		return Result(0, false);
+	}
+	// Find Random tsume
+	Result FindRandomMove(const Stone turn) {
+		for (size_t p = 0; p < kBoardSize * kBoardSize; ++p) {
+			if (board_[p] != Stone::None) continue;
+			for (uint8_t dir = 0; dir < Direction::Directions; ++dir) {
+				auto move_pattern = GetPattern(p, static_cast<Direction>(dir));
+				continue;
+			}
+		}
+		return Result(0, false);
+	}
+public:
+	// constructor
+	Board(const char board_text[], const char turn_text[]) {
+		const static string kStoneString = "-*O";
+		// Read board-text
+		const string board_text_str(board_text);
+		if (board_text_str.size() < kBoardSize * kBoardSize) {
+			throw std::exception("Too short board-text size!");
+		}
+		for (size_t p = 0; p < kBoardSize * kBoardSize; ++p) {
+			auto stone_type = kStoneString.find_first_of(board_text[p], 0);
+			if (stone_type == string::npos) {
+				throw std::exception("Can't read board data.");
+			}
+			board_[p] = static_cast<Stone>(stone_type);
+		}
+		// Read turn-text
+		auto turn_type = kStoneString.find_first_of(turn_text[0], 0);
+		if (turn_type == string::npos) {
+			throw std::exception("Can't read turn data.");
+		}
+		turn_ = static_cast<Stone>(turn_type);
+		// Initialize IterateTable
+		for (size_t y = 0; y < kBoardSize; ++y) {
+			for (size_t x = 0; x < kBoardSize; ++x) {
+				auto p = ToPosition(x, y);
+				// Row(Ñü)
+				kIterateTable[p][Direction::Row][Side::Right] = std::min(kBoardSize - x - 1, kSearchWidth);
+				kIterateTable[p][Direction::Row][Side::Left ] = std::min(x, kSearchWidth);
+				// Column(Ñ†)
+				kIterateTable[p][Direction::Column][Side::Right] = std::min(kBoardSize - y - 1, kSearchWidth);
+				kIterateTable[p][Direction::Column][Side::Left ] = std::min(y, kSearchWidth);
+				// Diagonally right(Å^)
+				kIterateTable[p][Direction::DiagR][Side::Right] = std::min(std::min(x, kBoardSize - y - 1), kSearchWidth);
+				kIterateTable[p][Direction::DiagR][Side::Left ] = std::min(std::min(kBoardSize - x - 1, y), kSearchWidth);
+				// Diagonally left(Å_)
+				kIterateTable[p][Direction::DiagL][Side::Right] = std::min(std::min(kBoardSize - x - 1, kBoardSize - y - 1), kSearchWidth);
+				kIterateTable[p][Direction::DiagL][Side::Left ] = std::min(std::min(x, y), kSearchWidth);
+			}
+		}
+	}
 	// Thinking next move
 	int NextMove(const size_t depth, bool debug_flg = false) {
 		// Opening move
@@ -268,8 +353,11 @@ public:
 		result = FindGorenMove(EnemyTurn(turn_));
 		if (result.second) return result.first;
 		// Search of quadruplex's problem(Shi-Oi Tsume)
-
-		//PutBoard();
+		result = FindShioiMove(EnemyTurn(turn_));
+		if (result.second) return result.first;
+		// Random move(test code)
+		result = FindRandomMove(EnemyTurn(turn_));
+		if (result.second) return result.first;
 		return -1;
 	}
 };
