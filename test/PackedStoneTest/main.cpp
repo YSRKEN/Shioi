a347bb52a53e630667057f526455e37f5ed98328
yumetodo <yume-wikijp@live.jp>
2016-04-19 08:57:27 +0900

    PackedStoneクラスのテストケースを書き始める

---
+++ test/PackedStoneTest/main.cpp
@@ -0,0 +1,8 @@
+﻿#include "../../I401/PackedStone.hpp"
+int main() {
+	static_assert((Stone::Black**5_pack).data() == 0b0101010101, "error");
+	static_assert((Stone::White | Stone::None**2_pack | Stone::White).data() == 0b10000010, "error");
+	static_assert(Stone::Black**4_pack == (Stone::Black | Stone::Black | Stone::Black | Stone::Black), "error");
+	static_assert((Stone::Black | Stone::Black | Stone::Black | Stone::White).front() == Stone::Black, "error");
+	static_assert((Stone::Black | Stone::Black | Stone::Black | Stone::White).back() == Stone::White, "error");
+}
